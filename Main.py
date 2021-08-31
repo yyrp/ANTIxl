2a050ed7b80af18312ccdd944b0b2b51de19e488
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-08-30 21:16:28 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+try:
+    import pyautogui as pygui
+except:
+    print("You need to install the pyautogui module (pip install pyautogui).")
+    sys.exit()
+
+try:
+    import tkinter as tk
+except:
+    print("You need to install the tkinter module (pip install tkinter).")
+    sys.exit()
+
+try:
+    import cv2
+except:
+    print("You need to install the cv2 module (pip install opencv-python).")
+    sys.exit()
+
+try:
+    import pytesseract
+except:
+    print("You need to install the pytesseract module (pip install pytesseract).")
+    sys.exit()
+
+import time
+import sys
+import os
