ad0732c33223fc23ad7613f60969c0394c7ad94c
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-10-13 21:26:50 -0400

    Made the checking for libraries thing better.
    It's better but there's this one bug that I can't fix.

--- Main.py
+++ Main.py
@@ -5,26 +5,73 @@ print("Importing modules...")
 
 import sys
 import os
+import platform
 
+# These variables are used to see what libraries are or aren't installed
+pygui_error = False
+cv2_error = True
+pytesseract_error = False
+
+# This is the function checks what libraries need to be installed
+def library_install_check():
+    libraries_that_need_installed = []
+
+    if platform.system() == "Windows":
+        # This adds color to the text if the os is Windows becuase I can't get it to work on Linux
+        if pygui_error == True:
+            libraries_that_need_installed.append("  - \u001b[32mpyautogui\u001b[37m (pip install pyautogui)")
+        if cv2_error == True:
+            libraries_that_need_installed.append("  - \u001b[32mcv2\u001b[37m (pip install opencv-python)")
+            print("food")
+        if pytesseract_error == True:
+            libraries_that_need_installed.append("  - \u001b[32mpytesseract\u001b[37m (pip install pytesseract)")
+
+            print()
+            print(" \u001b[31mERROR:\u001b[37m You need to install the following libraries:")
+            print(libraries_that_need_installed)
+            print()
+            print()
+    else:
+        # This is for any other operating system besides Windows
+        if pygui_error == True:
+            libraries_that_need_installed.append("  - pyautogui (pip install pyautogui)")
+        if pytesseract_error == True:
+            libraries_that_need_installed.append("  - pytesseract (pip install pytesseract)")
+        if cv2_error == True:
+            libraries_that_need_installed.append("  - cv2 (pip install opencv-python)")
+
+            print()
+            print(" ERROR: You need to install the following libraries:")
+            print("\n".join(libraries_that_need_installed))
+            print()
+            print()
+
+    sys.exit()
+
+
+# This sees what libraries are or aren't installed
 try:
     import pyautogui as pygui
 except:
-    print("You need to install the pyautogui module (pip install pyautogui).")
-    sys.exit()
-# ------------------------This will be removed later----------------------------
+    pygui_error = True
+# ------------------------This will probably be removed later-------------------
 try:
     import cv2
 except:
-    print("You need to install the cv2 module (pip install opencv-python).")
-    sys.exit()
+    cv2_error = True
 
 try:
     import pytesseract
 except:
-    print("You need to install the pytesseract module (pip install pytesseract).")
-    sys.exit()
+    pygui_error = True
 
 # ------------------------------------------------------------------------------
+if platform.system() == "Windows":
+    os.system('cls')
+elif platform.system() == "Linux":
+    os.system('clear')
+
+library_install_check()
 
 cd = os.getcwd()
 sys.path.insert(1, cd+'/Data/ExternalModules')
